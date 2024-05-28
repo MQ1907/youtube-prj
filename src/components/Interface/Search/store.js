aacc1e4ada7c66701f8fa0b53fd95ecd4dcda558
minhquan <dinhbaminhquan69@gmail.com>
2024-05-28 11:06:05 +0700

    lennn bai

--- src/components/Interface/Search/store.js
+++ src/components/Interface/Search/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { searchVideosReducer } from "./videos.reducer";
-import { thunk } from "redux-thunk";
+import  thunk  from "redux-thunk";
 
 const rootReducer = combineReducers({
   searchVideo: searchVideosReducer,
